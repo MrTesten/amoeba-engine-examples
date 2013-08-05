cfb8e27938e61602bd542855c28885d19f1e3868
jsantangelo <jamesfsantangelo@gmail.com>
2013-08-05 19:49:56 -0400

    [#3] Removed View nonsense, replaced with TextSprite. Added initial conversion to use built-in text features.

--- InputExample/src/org/amoeba/examples/inputexample/InputExample.java
+++ InputExample/src/org/amoeba/examples/inputexample/InputExample.java
@@ -1,91 +1,81 @@
 package org.amoeba.examples.inputexample;
 
 import org.amoeba.activity.GameActivity;
-import org.amoeba.graphics.camera.Camera;
 import org.amoeba.engine.service.input.InputEvent;
+import org.amoeba.entity.sprite.TextSprite;
+import org.amoeba.graphics.camera.Camera;
+import org.amoeba.graphics.texture.TextOptions;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputExample extends GameActivity
 {
 	private static final String TAG = "AmoebaEngine.InputExample";
 
-    private TextView eventList = null;
+    private List<TextSprite> information;
+    private TextSprite eventList;
 
     @Override
     public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        //Removing the Engine's GLSurfaceView's parent, so it can be reused in another layout.
-        ((ViewGroup) ((GLSurfaceView) getView()).getParent()).removeView((GLSurfaceView) getView());
+        information = new ArrayList<TextSprite>();
 
-        RelativeLayout relativeLayout = new RelativeLayout(this);
-        relativeLayout.addView((GLSurfaceView) getView());
-        eventList = new TextView(this);
-        eventList.setMovementMethod(new ScrollingMovementMethod());
-        eventList.setWidth(120);
+        TextOptions informationOptions = new TextOptions(12, Color.WHITE, Align.CENTER, Typeface.DEFAULT, true);
+        information.add(getGraphicsService().getTextFactory().createTextSprite(
+            "Touch the screen to see data", informationOptions));
+        information.add(getGraphicsService().getTextFactory().createTextSprite(
+            "on your inputs. Use the HOME", informationOptions));
+        information.add(getGraphicsService().getTextFactory().createTextSprite(
+            "key to exit the application.", informationOptions));
 
-        LayoutParams layoutParameters = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        layoutParameters.addRule(RelativeLayout.ALIGN_TOP);
-        eventList.setLayoutParams(layoutParameters);
-        eventList.setText("Touch the screen to see data on your inputs. Use the HOME key to exit the application.\n\n");
-        eventList.setBackgroundColor(0x07000000);
-        relativeLayout.addView(eventList);
+        TextOptions eventListOptions = new TextOptions(48, Color.WHITE, Align.CENTER, Typeface.DEFAULT, true);
+        eventList = getGraphicsService().getTextFactory().createTextSprite("Some Event", eventListOptions);
+    }
 
-        setContentView(relativeLayout);
+    @Override
+    public void onSurfaceChanged(final int width, final int height)
+    {
+        int informationSpriteHeight = 10;
+        for (TextSprite informationText : information) {
+            informationText.setPosition(width/2, informationSpriteHeight);
+            informationSpriteHeight += 12;
+        }
+        
+        eventList.setPosition(width/2, 60);
+        eventList.setWidth(40);
+        eventList.setHeight(40);
     }
 
     @Override
     public void onDraw(final Camera camera)
     {
+        super.onDraw(camera);
         GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1f);
     }
 
     @Override
 	public void onInputEvent(final InputEvent event)
 	{
-        eventList.append(event.getEventType().toString() + "\n");
-        scrollText();
+
 	}
 
     @Override
     public void onBackPressed() {
-        eventList.append("BACK KEY PRESSED\n");
-        scrollText();
-
         MediaPlayer smbCoinSound = MediaPlayer.create(this, R.raw.smw_coin);
         smbCoinSound.start();
     }
 
-    private void scrollText() {
-        eventList.post(new Runnable()
-        {
-            public void run()
-            {
-                final int scrollAmount = eventList.getLayout().getLineTop(eventList.getLineCount())
-                    - eventList.getHeight();
-
-                if (scrollAmount > 0)
-                {
-                    eventList.scrollTo(0, scrollAmount);
-                }
-                else
-                {
-                    eventList.scrollTo(0, 0);
-                }    
-            }
-        });
-    }
 }
